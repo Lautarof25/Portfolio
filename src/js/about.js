17169e6dcfe1e0617dd5bb2092d352010325a0dd
Lautaro Exequiel Fernández <81919816+Lautarof25@users.noreply.github.com>
2023-10-18 13:57:02 -0300

    adding js transition to sections and effects

--- src/js/about.js
+++ src/js/about.js
@@ -1,66 +1,73 @@
 const aboutInfo = {
   "info": [
     {
-      "description": "Soy técnico en mantenimiento y armado de PC del IAC.",
-      "icons": ["fa-computer", "fa-screwdriver-wrench"]
+      "description": "Soy técnico en mantenimiento y armado de PC certificado del IAC.",
+      "icons": ["fa-solid fa-computer", "fa-solid fa-screwdriver-wrench"]
     },
     {
       "description": "Estudio programación desde mediados del 2020",
-      "icons": ["fa-pencil", "fa-code"]
+      "icons": ["fa-solid fa-pencil", "fa-solid fa-code"]
     },
     {
       "description": "Tengo conocimientos en HTML, CSS, JS y algunos frameworks (Bootstrap y Tailwind)",
-      "icons": ["fa-html5", "fa-css5","fa-js","fa-bootstrap"]
+      "icons": ["fa-brands fa-html5", "fa-brands fa-css3","fa-brands fa-js","fa-brands fa-bootstrap"]
     },
     {
       "description": "También conozco Python y Java, y estoy estudiando ReactJS, Spring, PHP, entre otros.",
-      "icons": ["fa-python", "fa-java","fa-react","fa-php"]
+      "icons": ["fa-brands fa-python", "fa-brands fa-java","fa-brands fa-react","fa-brands fa-php"]
     },
     {
       "description": "Me interesa también la parte de testing QA y bases de datos.",
-      "icons": ["fa-bug", "fa-database"]
+      "icons": ["fa-solid fa-bug", "fa-solid fa-database"]
     },
   ]
 }
 
-const databaseAboute = aboutInfo.info
+const about = document.querySelector('.about');
+
+const databaseAbout = aboutInfo.info
 
 function addCardInfo(){
-  databaseAboute.forEach(item => {
-
+  databaseAbout.forEach(item => {
+    createCardInfo(item.description, item.icons)
   })
 }
 
-function createCardInfo(length){
+function createCardInfo(description, icons){
   const div = document.createElement("div")
+  div.setAttribute("class","section__box mw-500 relative p-05 m-0-auto")
 
   const divRight = document.createElement("div")
+  divRight.setAttribute("class","bg-principal-color absolute w-50px h-50px top-0 right-0 border-radius-5")
   const divRightOffset = document.createElement("div")
+  divRightOffset.setAttribute("class","bg-black absolute w-100 h-100 topOffset rightOffset border-radius-5")
+  divRight.appendChild(divRightOffset)
 
   const divLeft = document.createElement("div")
+  divLeft.setAttribute("class", "bg-principal-color absolute w-50px h-50px zindex-1 left-0 bottom-0 border-radius-5")
   const divLeftOffset = document.createElement("div")
+  divLeftOffset.setAttribute("class", "bg-black absolute w-100 h-100 bottomOffset leftOffset border-radius-5")
+  divLeft.appendChild(divLeftOffset)
 
   const divContainer = document.createElement("div")
+  divContainer.setAttribute("class", "relative zindex-1 d-flex flex-column p-1 align-center")
   const p = document.createElement("p")
-  const divIcons = document.createElement("div")  
-  const icon = document.createElement("i")
+  p.setAttribute("class", "section__description text-center")
+  const pText = document.createTextNode(`${description}`)
+  p.appendChild(pText)
+  const divIcons = document.createElement("div") 
+  divIcons.setAttribute("class", "section__icons d-flex gap-05")
 
+  divContainer.append(p,divIcons)
+
+  for (let i = 0; i < icons.length; i++) {
+    const icon = document.createElement("i")
+    icon.setAttribute("class", `${icons[i]} fa-2xl`)
+    divIcons.appendChild(icon)
+  }
   
+  div.append(divRight,divLeft,divContainer)
+  about.appendChild(div)
 }
 
-{/* 
-<div class="section__box mw-500 relative p-05 m-0-auto">
-  <div class="bg-principal-color absolute w-50px h-50px top-0 right-0 border-radius-5">
-    <div class="bg-black absolute w-100 h-100 topOffset rightOffset border-radius-5"></div>
-  </div>
-  <div class="bg-principal-color absolute w-50px h-50px zindex-1 left-0 bottom-0 border-radius-5">
-    <div class="bg-black absolute w-100 h-100 bottomOffset leftOffset border-radius-5"></div>
-  </div>
-  <div class="relative zindex-1 d-flex flex-column p-1 align-center">
-    <p class="section__description text-center">Me interesa también la parte de testing QA y bases de datos.</p>
-    <div class="section__icons">
-      <i class="fa-solid fa-bug fa-2xl"></i>
-      <i class="fa-solid fa-database fa-2xl"></i>
-    </div>
-  </div> 
-</div>*/}
+addCardInfo()
--- src/js/linksSelector.js
+++ src/js/linksSelector.js
@@ -4,9 +4,17 @@ links.forEach(link => {
     link.addEventListener('click',function(){
         removeClassLink(links)
         link.classList.add('link-active');
+        addClassHidden(links)
+        document.querySelector(`.${link.id}`).classList.remove('hidden');
     })
 })
 
 function removeClassLink(links){
     links.forEach(link => link.classList.remove('link-active'));
+}
+
+function addClassHidden(links){
+    links.forEach(link => {
+        document.querySelector(`.${link.id}`).classList.add('hidden');
+    })
 }
