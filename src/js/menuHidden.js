04bd39339624a571f7bd88b181c06caaedad682a
Lautaro Exequiel Fernández <81919816+Lautarof25@users.noreply.github.com>
2023-10-18 20:31:42 -0300

    adding footer

--- src/js/menuHidden.js
+++ src/js/menuHidden.js
@@ -1,29 +1,35 @@
 const header = document.querySelector("header");
 const nav = document.querySelector("nav");
+const footer = document.querySelector("footer");
 const navIcons = document.querySelectorAll(".nav i")
+const footerIcons = document.querySelectorAll(".footer a")
 const main = document.querySelector("main");
 
 function handleMouseOver() {
   header.classList.add("show");
   nav.classList.add("show");
-  removeIcons()
+  footer.classList.add("show");
+  removeIcons(navIcons)
+  removeIcons(footerIcons)
   main.classList.add("blur");
 }
 
 function handleMouseOut() {
   header.classList.remove("show");
   nav.classList.remove("show");
-  hiddenIcons()
+  footer.classList.remove("show");
+  hiddenIcons(navIcons)
+  hiddenIcons(footerIcons)
   main.classList.remove("blur");
 }
 
-function hiddenIcons(){
-  navIcons.forEach(icon => {
+function hiddenIcons(element){
+  element.forEach(icon => {
     icon.classList.add("hidden-icon");
   })
 }
-function removeIcons(){
-  navIcons.forEach(icon => {
+function removeIcons(element){
+  element.forEach(icon => {
     icon.classList.remove("hidden-icon");
   })
 }
@@ -33,3 +39,5 @@ header.addEventListener("mouseover", handleMouseOver);
 header.addEventListener("mouseout", handleMouseOut);
 nav.addEventListener("mouseover", handleMouseOver);
 nav.addEventListener("mouseout", handleMouseOut);
+footer.addEventListener("mouseover", handleMouseOver);
+footer.addEventListener("mouseout", handleMouseOut);
