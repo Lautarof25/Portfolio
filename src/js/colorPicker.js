b157e41e2047d8957cc080dab15a2e94a834af74
Lautaro Exequiel Fernández <81919816+Lautarof25@users.noreply.github.com>
2023-10-28 21:40:24 -0300

    adding a color picker

---
+++ src/js/colorPicker.js
@@ -0,0 +1,11 @@
+const body = document.querySelector("body")
+const radios = document.querySelectorAll('[type="radio"]')
+
+radios.forEach(radio => {
+  radio.addEventListener("change", function(){
+    body.style.setProperty("--principal-color", radio.value)
+  })
+})
+
+
+
