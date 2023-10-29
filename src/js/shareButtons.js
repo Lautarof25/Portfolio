802cd188486ff40563a6e93c4705cd51eecac4a0
Lautaro Exequiel Fernández <81919816+Lautarof25@users.noreply.github.com>
2023-10-29 16:12:37 -0300

    fixing share buttons

--- src/js/shareButtons.js
+++ src/js/shareButtons.js
@@ -1,11 +1,12 @@
-const shareButtons = document.querySelector(".shareButtons")
+const shareButtons = document.querySelectorAll(".share-button")
+const containerButton = document.querySelector(".share-buttons")
 const closeButton = document.querySelector(".closeButton")
 
 let countClicks = 0
 
-addEventListener('click',function(){
+addEventListener('click', function () {
     countClicks++
-    if(countClicks === 5){
+    if (countClicks === 5) {
         header.classList.add('blur-effect')
         nav.classList.add('blur-effect')
         footer.classList.add('blur-effect')
@@ -13,12 +14,12 @@ addEventListener('click',function(){
         titlePage.classList.add('blur-effect')
         buttonDown.classList.add('blur-effect')
         buttonUp.classList.add('blur-effect')
-        shareButtons.classList.remove("hidden")
+        containerButton.classList.remove("hidden")
     }
 })
 
-closeButton.addEventListener('click',function(){
-    shareButtons.classList.add('hidden')
+closeButton.addEventListener('click', function () {
+    containerButton.classList.add('hidden')
     header.classList.remove('blur-effect')
     nav.classList.remove('blur-effect')
     footer.classList.remove('blur-effect')
@@ -26,4 +27,41 @@ closeButton.addEventListener('click',function(){
     titlePage.classList.remove('blur-effect')
     buttonDown.classList.remove('blur-effect')
     buttonUp.classList.remove('blur-effect')
+});
+
+// Add click event listener to each button
+shareButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        // Get the URL of the current page
+        const url = window.location.href;
+
+        // Get the social media platform from the button's class name
+        const platform = button.classList[1];
+
+        // Set the URL to share based on the social media platform
+        let shareUrl;
+        switch (platform) {
+            case 'facebook':
+                shareUrl = `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(url)}`;
+                break;
+            case 'twitter':
+                shareUrl = `https://twitter.com/share?url=${encodeURIComponent(url)}`;
+                break;
+            case 'linkedin':
+                shareUrl = `https://www.linkedin.com/shareArticle?url=${encodeURIComponent(url)}`;
+                break;
+            case 'pinterest':
+                shareUrl = `https://pinterest.com/pin/create/button/?url=${encodeURIComponent(url)}`;
+                break;
+            case 'reddit':
+                shareUrl = `https://reddit.com/submit?url=${encodeURIComponent(url)}`;
+                break;
+            case 'whatsapp':
+                shareUrl = `https://api.whatsapp.com/send?text=${encodeURIComponent(url)}`;
+                break;
+        }
+
+        // Open a new window to share the URL
+        window.open(shareUrl, '_blank');
+    });
 });
