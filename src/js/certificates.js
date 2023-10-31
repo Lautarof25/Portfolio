b39267adb9f0915cb57179f6e2d04154cddb97ce
Lautaro Exequiel Fernández <81919816+Lautarof25@users.noreply.github.com>
2023-10-30 22:37:00 -0300

    refactoring

--- src/js/certificates.js
+++ src/js/certificates.js
@@ -5,6 +5,7 @@ const certificatesInfo = {
       "description": "JavaScritp Essentially 1",
       "thumbnail": "cisco.webp",
       "hours": 40,
+      "year": 2023,
       "link": "https://www.credly.com/badges/85c38784-390e-4e55-8dcc-94e447fd32b0/linked_in?t=s0151l",
     },
     {
@@ -12,6 +13,7 @@ const certificatesInfo = {
       "description": "Responsive web design",
       "thumbnail": "freeCodeCamp.webp",
       "hours": 300,
+      "year": 2022,
       "link": "https://freecodecamp.org/certification/lautarof/responsive-web-design",
     },
     {
@@ -19,6 +21,7 @@ const certificatesInfo = {
       "description": "Testing de software Básico",
       "thumbnail": "academia_qa_testing.webp",
       "hours": 20,
+      "year": 2022,
       "link": "https://academiaqa.com/Certificado/9ea932d7a7265fedc49ddb5ae172cdf7/?swcfpc=1",
     },
     {
@@ -26,6 +29,7 @@ const certificatesInfo = {
       "description": "Introducción al desarrollo web II",
       "thumbnail": "google_activate.webp",
       "hours": 40,
+      "year": 2021,
       "link": "https://learndigital.withgoogle.com/link/1nur091p2ww",
     },
     {
@@ -33,6 +37,7 @@ const certificatesInfo = {
       "description": "Test Typing Speed",
       "thumbnail": "ratatype.webp",
       "hours": 0.01,
+      "year": 2023,
       "link": "https://www.ratatype.com/certificates/en/es/9/3/2/5476932.webp?1698681240",
     },
     {
@@ -40,6 +45,7 @@ const certificatesInfo = {
       "description": "English Test",
       "thumbnail": "ef_test_english.jpg",
       "hours": 1,
+      "year": 2023,
       "link": "https://www.efset.org/cert/KDPqio",
     },
   ]
@@ -51,11 +57,11 @@ const databaseCertificates = certificatesInfo.info
 
 function addCardCertificatesInfo() {
   databaseCertificates.forEach(item => {
-    createCardCertificatesInfo(item.title, item.description, item.thumbnail, item.hours, item.link)
+    createCardCertificatesInfo(item.title, item.description, item.thumbnail, item.hours,item.year, item.link)
   })
 }
 
-function createCardCertificatesInfo(title, description, thumbnail, hours, link) {
+function createCardCertificatesInfo(title, description, thumbnail, hours, year, link) {
   const div = document.createElement("div")
   div.setAttribute("class", "section__box mw-500 relative p-05 m-0-auto w-100")
 
@@ -103,9 +109,13 @@ function createCardCertificatesInfo(title, description, thumbnail, hours, link) 
   const pText = document.createTextNode(`${description}`)
   p.appendChild(pText)
   const pHours = document.createElement("p")
-  const phoursText = document.createTextNode(`Horas: ${hours}`)
-  pHours.appendChild(phoursText)
+  const pHoursText = document.createTextNode(`Horas: ${hours}`)
+  pHours.appendChild(pHoursText)
   pHours.setAttribute("class", "m-0")
+  const pYear = document.createElement("p")
+  const pYearText = document.createTextNode(`Año: ${year}`)
+  pYear.appendChild(pYearText)
+  pYear.setAttribute("class", "m-0")
   const aLink = document.createElement("a")
   const aLinkText = document.createTextNode("Link")
   aLink.appendChild(aLinkText)
@@ -120,7 +130,7 @@ function createCardCertificatesInfo(title, description, thumbnail, hours, link) 
   img.setAttribute("alt", `${title}`)
   img.setAttribute("class", "w-100")
   aLinkImg.appendChild(img)
-  divContainerText.append(h2, p, pHours, aLink, aLinkImg)
+  divContainerText.append(h2, p, pHours, pYear, aLink, aLinkImg)
 
   div.append(divRightTop, divRightBottom, divLeftTop, divLeftBottom, divContainerText)
   certificates.appendChild(div)
