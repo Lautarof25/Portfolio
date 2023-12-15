9dc05d4148dd6be4fe0b1e5de3f890fee77b49c9
Lautaro Exequiel Fernández <81919816+Lautarof25@users.noreply.github.com>
2023-12-15 17:32:25 -0300

    adding button cv home page

--- src/js/scrollNavigation.js
+++ src/js/scrollNavigation.js
@@ -1,56 +1,56 @@
-let topPageCount = 0;
-let endPageCount = 0;
-let startY;
+let topPageCount = 0
+let endPageCount = 0
+let startY
 
 const handleScroll = (event) => {
     const isScrollDown = event.deltaY > 0;
-    isScrollDown ? endPageCount++ : topPageCount++;
+    isScrollDown ? endPageCount++ : topPageCount++
 
     if (endPageCount >= 10) {
-        const totalHeight = document.body.scrollHeight;
-        const visibleHeight = window.innerHeight;
-        const currentScrollPos = window.scrollY;
+        const totalHeight = document.body.scrollHeight
+        const visibleHeight = window.innerHeight
+        const currentScrollPos = window.scrollY
 
         if (currentScrollPos + visibleHeight >= totalHeight) {
-            endPageCount = 0;
-            navigatePage(1);
-            scrollToTop();
+            endPageCount = 0
+            navigatePage(1)
+            scrollToTop()
         }
     }
 
     if (window.scrollY === 0 && topPageCount >= 5) {
-        topPageCount = 0;
-        navigatePage(-1);
-        scrollToTop();
+        topPageCount = 0
+        navigatePage(-1)
+        scrollToTop()
     }
-};
+}
 
 const handleTouchStart = (event) => {
-    startY = event.touches[0].clientY;
-};
+    startY = event.touches[0].clientY
+}
 
 const handleTouchMove = (event) => {
-    const currentY = event.touches[0].clientY;
-    startY > currentY ? endPageCount++ : topPageCount++;
+    const currentY = event.touches[0].clientY
+    startY > currentY ? endPageCount++ : topPageCount++
 
-    const totalHeight = document.body.scrollHeight;
-    const visibleHeight = window.innerHeight;
-    const currentScrollPos = window.scrollY;
+    const totalHeight = document.body.scrollHeight
+    const visibleHeight = window.innerHeight
+    const currentScrollPos = window.scrollY
 
     if (endPageCount >= 5) {
         if (currentScrollPos + visibleHeight >= totalHeight) {
-            endPageCount = 0;
-            navigatePage(1);
-            scrollToTop();
+            endPageCount = 0
+            navigatePage(1)
+            scrollToTop()
         }
     }
 
     if (topPageCount >= 5 && window.scrollY === 0) {
-        topPageCount = 0;
-        navigatePage(-1);
-        scrollToTop();
+        topPageCount = 0
+        navigatePage(-1)
+        scrollToTop()
     }
-};
+}
 
 const scrollToTop = () => {
     setTimeout(() => {
@@ -58,11 +58,11 @@ const scrollToTop = () => {
             top: 0,
             left: 0,
             behavior: "smooth",
-        });
-    }, 700);
-};
+        })
+    }, 700)
+}
 
 // Add event listeners
-window.addEventListener("wheel", handleScroll);
-document.addEventListener("touchstart", handleTouchStart);
-document.addEventListener("touchmove", handleTouchMove);
+window.addEventListener("wheel", handleScroll)
+document.addEventListener("touchstart", handleTouchStart)
+document.addEventListener("touchmove", handleTouchMove)
