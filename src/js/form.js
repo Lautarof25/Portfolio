15416c09969fd6246d9dea39d56452b918a8b053
Lautaro Exequiel Fernández <81919816+Lautarof25@users.noreply.github.com>
2023-12-24 16:46:39 -0300

    starting email validation, interactivity placeholder

--- src/js/form.js
+++ src/js/form.js
@@ -5,3 +5,30 @@ function clearForm() {
 }
 
 contact.addEventListener("click", clearForm)
+
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+// Ejemplo de uso:
+const emailInput = document.getElementById('email');
+const emailValue = emailInput.value.trim();
+
+const namePlaceHolder = "Lautaro Fernández"
+const emailPlaceHolder = "Fernandez.lautaro25@gmail.com"
+const messagePlaceHolder = "👋​ Hi Lautaro, I really like your website!😍​"
+
+let contactPageActive = false
+
+addEventListener("change",function(){
+    // if(currentIdPage() === "contact"){
+    //     contactPageActive = true
+    // }
+    console.log("Estoy en la página "+ currentIdPage());
+})
+
+const nameCompleter = setInterval(() => {
+    if (contactPageActive) {
+        
+    }else {
+        clearInterval(nameCompleter)
+    }
+}, 50);
