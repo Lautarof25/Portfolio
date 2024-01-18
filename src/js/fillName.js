26a7c637affad5b34e76e82cef6228e492950098
Lautaro Exequiel Fernández <81919816+Lautarof25@users.noreply.github.com>
2024-01-18 14:08:42 -0300

    adding cursor blink name and changing color buttons

--- src/js/fillName.js
+++ src/js/fillName.js
@@ -1,36 +1,36 @@
 document.addEventListener('DOMContentLoaded', function () {
-    let words = ['Lautaro', 'Exequiel', 'Fernández']
-    let index = 0
-    let currentLetter = 0
-    let direction = 1
-    let speedWriting = 250 // milisegundos por letra
-    let speedChangeWords = 1000 // milisegundos entre cambios de palabra
+  let words = ['Lautaro', 'Exequiel', 'Fernández']
+  let index = 0
+  let currentLetter = 0
+  let direction = 1
+  let speedWriting = 250 // milisegundos por letra
+  let speedChangeWords = 1000 // milisegundos entre cambios de palabra
 
-    function writeDeleteWords() {
+  function writeDeleteWords() {
       let currentWord = words[index]
       let element = document.querySelector('.nameChange')
 
       if (direction === 1) {
-        element.textContent = currentWord.substring(0, currentLetter)
-        currentLetter++
-        if (currentLetter > currentWord.length) {
-          direction = -1
-          setTimeout(writeDeleteWords, speedChangeWords)
-        } else {
-          setTimeout(writeDeleteWords, speedWriting)
-        }
+          element.innerHTML = currentWord.substring(0, currentLetter) + '<span class="blinking-cursor">|</span>'
+          currentLetter++
+          if (currentLetter > currentWord.length) {
+              direction = -1
+              setTimeout(writeDeleteWords, speedChangeWords)
+          } else {
+              setTimeout(writeDeleteWords, speedWriting)
+          }
       } else {
-        element.textContent = currentWord.substring(0, currentLetter)
-        currentLetter--
-        if (currentLetter === 0) {
-          direction = 1
-          index = (index + 1) % words.length
-          setTimeout(writeDeleteWords, speedChangeWords)
-        } else {
-          setTimeout(writeDeleteWords, speedWriting / 2)
-        }
+          element.innerHTML = currentWord.substring(0, currentLetter) + '<span class="blinking-cursor">|</span>'
+          currentLetter--
+          if (currentLetter === 0) {
+              direction = 1
+              index = (index + 1) % words.length
+              setTimeout(writeDeleteWords, speedChangeWords)
+          } else {
+              setTimeout(writeDeleteWords, speedWriting / 2)
+          }
       }
-    }
+  }
 
-    writeDeleteWords()
+  writeDeleteWords()
 })
