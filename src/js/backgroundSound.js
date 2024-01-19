c9baa6881726f07a55f74dd606d46c36920f68cc
Lautaro Exequiel Fernández <81919816+Lautarof25@users.noreply.github.com>
2024-01-19 20:56:23 -0300

    adding alt logos images

--- src/js/backgroundSound.js
+++ src/js/backgroundSound.js
@@ -1,7 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     const audio = document.getElementById("audio")
     audio.volume = 0.2
-    audio.autoplay = true
     audio.load()
 
     const playButton = document.querySelector(".sound")
