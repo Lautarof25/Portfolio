af997307aff8a1595bb0979db6a6457137b8a330
Lautaro Exequiel Fernández <81919816+Lautarof25@users.noreply.github.com>
2024-01-23 11:47:31 -0300

    update for functional page(without checkboxes)

--- src/js/filterCheckboxes.js
+++ src/js/filterCheckboxes.js
@@ -3,38 +3,38 @@ const checkboxesCertificates = $$('.certificates input[type="checkbox"]')
 let checkboxesPortfolioChecked = []
 let checkboxesCertificatesChecked = []
 
-const getCheckedDefault = () => {
-    checkboxesPortfolio.forEach(checkbox => {
-        if (checkbox.checked) {
-            checkboxesPortfolioChecked.push(checkbox.value.slice(-4))
-        }
-    })
-}
+// const getCheckedDefault = () => {
+//     checkboxesPortfolio.forEach(checkbox => {
+//         if (checkbox.checked) {
+//             checkboxesPortfolioChecked.push(checkbox.value.slice(-4))
+//         }
+//     })
+// }
 
-getCheckedDefault()
+// getCheckedDefault()
 
-const addChecked = (ckbs, array) => {
-    ckbs.forEach(ckb => {
-        ckb.addEventListener('change', () => {
-            if (ckb.checked) {
-                array.push(ckb.value.slice(-4))
-            } else {
-                array = array.filter(function (i) { return i !== ckb.value.slice(-4) });
-            }
-            array.sort((a, b) => b - a)
-            removeElements()
-            addCardInfo(currentIdPage(), eval(`${currentIdPage()}Info.info`))
-        })
-    })
-}
+// const addChecked = (ckbs, array) => {
+//     ckbs.forEach(ckb => {
+//         ckb.addEventListener('change', () => {
+//             if (ckb.checked) {
+//                 array.push(ckb.value.slice(-4))
+//             } else {
+//                 array = array.filter(function (i) { return i !== ckb.value.slice(-4) });
+//             }
+//             array.sort((a, b) => b - a)
+//             removeElements()
+//             addCardInfo(currentIdPage(), eval(`${currentIdPage()}Info.info`))
+//         })
+//     })
+// }
 
-addChecked(checkboxesPortfolio, checkboxesPortfolioChecked)
-addChecked(checkboxesCertificates, checkboxesCertificatesChecked)
+// addChecked(checkboxesPortfolio, checkboxesPortfolioChecked)
+// addChecked(checkboxesCertificates, checkboxesCertificatesChecked)
 
-function removeElements() {
-    var sectionBoxes = $$(`.${currentIdPage()} .section__box`);
+// function removeElements() {
+//     var sectionBoxes = $$(`.${currentIdPage()} .section__box`);
 
-    sectionBoxes.forEach(function (sectionBox) {
-        sectionBox.parentNode.removeChild(sectionBox);
-    });
-}   
+//     sectionBoxes.forEach(function (sectionBox) {
+//         sectionBox.parentNode.removeChild(sectionBox);
+//     });
+// }   
