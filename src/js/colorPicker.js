42252eea3c8e62c940cf61c0e3d8a5e287cd6615
Lautaro Exequiel Fernández <81919816+Lautarof25@users.noreply.github.com>
2024-01-24 18:51:55 -0300

    adding icon web dynamic

--- src/js/colorPicker.js
+++ src/js/colorPicker.js
@@ -1,35 +1,37 @@
 function handleRadioChange(radio) {
   if (radio.value === "default-color") {
-      body.style.setProperty("--principal-color", "#ff5b02");
-      body.style.cursor = `url("./src/img/cursor.cur") 4 12, auto`;
+    body.style.setProperty("--principal-color", "#ff5b02")
+    body.style.cursor = `url("./src/img/cursor.cur") 4 12, auto`
+    document.querySelector("link[rel~='icon']").href = "./src/img/iconweb.ico"
   } else {
-      body.style.setProperty("--principal-color", radio.value);
-      body.style.cursor = `url("./src/img/cursor${radio.value}.cur") 4 12, auto`;
+    body.style.setProperty("--principal-color", radio.value)
+    body.style.cursor = `url("./src/img/cursor${radio.value}.cur") 4 12, auto`
+    document.querySelector("link[rel~='icon']").href = `./src/img/${radio.value}.ico`
   }
 }
 
-colorPicker.addEventListener("mouseover", function(){
+colorPicker.addEventListener("mouseover", function () {
   colorPicker.style.bottom = "-80px"
 })
-colorPicker.addEventListener("mouseout", function(){
-  if(window.innerWidth <= 576){
+colorPicker.addEventListener("mouseout", function () {
+  if (window.innerWidth <= 576) {
     colorPicker.style.bottom = "-150px"
-  }else {
+  } else {
     colorPicker.style.bottom = "-180px"
   }
 })
 
 radios.forEach(radio => {
   radio.addEventListener("change", () => {
-      handleRadioChange(radio)
+    handleRadioChange(radio)
   })
   radio.addEventListener("focus", () => {
     colorPicker.style.bottom = "-80px"
   })
   radio.addEventListener("focusout", () => {
-    if(window.innerWidth <= 576){
+    if (window.innerWidth <= 576) {
       colorPicker.style.bottom = "-150px"
-    }else {
+    } else {
       colorPicker.style.bottom = "-180px"
     }
   })
