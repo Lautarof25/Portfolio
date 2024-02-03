9ed96aee408ca49780d7c2c3d1d97b256e361546
Lautaro Exequiel Fernández <81919816+Lautarof25@users.noreply.github.com>
2024-02-02 22:38:02 -0300

    styling

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,39 +1,53 @@
-allExceptAside.forEach(e => {
-    e.addEventListener('click', function () {
-        if (aside.classList.contains("show-aside")) {
-            aside.classList.remove("show-aside")
+const hideMenu = () =>{
+    allExceptAside.forEach(e => {
+        e.addEventListener('click', function () {
+            if (aside.classList.contains("show-aside")) {
+                aside.classList.remove("show-aside")
+                allExceptAside.forEach(element => {
+                    element.classList.remove("blur-effect")
+                })
+            }
+        })
+    })
+}
+
+hideMenu()
+
+const showMenu = () =>{
+    toggleMenu.addEventListener("click", function () {
+        aside.classList.toggle("show-aside")
+        allExceptAside.forEach(element => {
+            element.classList.toggle("blur-effect")
+        })
+    })
+}
+
+showMenu()
+
+const hideMenuAfterClick = () =>{
+    links.forEach(link => {
+        link.addEventListener("click", function () {
+            if (window.innerWidth <= 576) {
+                aside.classList.remove("show-aside")
+                allExceptShareButton.forEach(element => {
+                    element.classList.remove("blur-effect")
+                });
+            }
             allExceptAside.forEach(element => {
                 element.classList.remove("blur-effect")
-            });
+            })
+        })
+    })
+}
+hideMenuAfterClick()
+
+const hideMenuDestktopAction = () =>{
+    hideMenuDesktop.addEventListener("click", ()=>{
+        if(window.innerWidth >= 576) {
+            body.classList.toggle("removePaddingLeft")
+            aside.classList.toggle("hideMenuDesktop")
+            colorPicker.classList.toggle("removeMarginLeft")
         }
     })
-})
-
-toggleMenu.addEventListener("click", function () {
-    aside.classList.toggle("show-aside")
-    allExceptAside.forEach(element => {
-        element.classList.toggle("blur-effect")
-    });
-})
-
-links.forEach(link => {
-    link.addEventListener("click", function () {
-        if (window.innerWidth <= 576) {
-            aside.classList.remove("show-aside")
-            allExceptShareButton.forEach(element => {
-                element.classList.remove("blur-effect")
-            });
-        }
-        allExceptAside.forEach(element => {
-            element.classList.remove("blur-effect")
-        });
-    })
-})
-
-hideMenuDesktop.addEventListener("click", ()=>{
-    if(window.innerWidth >= 576) {
-        body.classList.toggle("removePaddingLeft")
-        aside.classList.toggle("hideMenuDesktop")
-        colorPicker.classList.toggle("removeMarginLeft")
-    }
-})
+}
+hideMenuDestktopAction()
